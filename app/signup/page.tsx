2ad5f853178d47253d22ae99416d3f2a4ccab83f
HagisawaYuki <hagisawa7110@gmail.com>
2025-05-01 22:02:35 +0900

    create login signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useState } from "react";
+import { searchAllUser } from "../api/user";
+import { Box, Text } from "@chakra-ui/react";
+
+type User = {
+  id: string;
+  name: string;
+  password: string;
+}
+
+export default function Home() {
+  const [users, setUsers] = useState<User[]>();
+  const init = async () => {
+    const _users = await searchAllUser();
+    setUsers(_users);
+
+  };
+
+  useEffect(() => {
+    init();
+  },[]);
+  
+  return (
+    <Box>
+      {users && <Text fontSize="2xl" color="blue">{users[0].name}</Text>
+
+      }
+      
+    </Box>
+  );
+}
