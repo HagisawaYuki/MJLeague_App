80938823304fdbbb79b85626a97c9de39155075d
HagisawaYuki <hagisawa7110@gmail.com>
2025-05-21 17:52:07 +0900

    update /mjscore

--- app/mjscore/page.tsx
+++ app/mjscore/page.tsx
@@ -11,28 +11,29 @@ export default function Home() {
     
   return (
     <Box display="flex" justifyContent="center">
-        <Box marginTop="5%">
+        <Box marginTop="5%" width="100%">
         {/* <Box bg="#EEE" padding="5%" marginTop="5%"> */}
+        <Table.ScrollArea borderWidth="1px" maxW="xl" width="90%">
             <Table.Root size="sm" variant="outline" showColumnBorder>
                 <Table.Header>
                     <Table.Row>
-                        <Table.ColumnHeader>
+                        <Table.ColumnHeader width="6.5%">
                             <Text>親</Text>
                         </Table.ColumnHeader>
                         {hus.map((hu) => (
-                            <Table.ColumnHeader key={hu}>{hu}</Table.ColumnHeader>
+                            <Table.ColumnHeader key={hu} width="8.5%">{hu}</Table.ColumnHeader>
                         ))}
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
                     {mj_scores.parent.map((hanSores) => (
                         <Table.Row key={hanSores.han}>
-                            <Table.Cell key={hanSores.han} bg="#EEE">{hanSores.han}</Table.Cell>
+                            <Table.Cell key={hanSores.han} bg="#EEE" width="6.5%">{hanSores.han}</Table.Cell>
                             {hanSores.scores.map((score) => (
-                                <Table.Cell key={score.hu}>
+                                <Table.Cell key={score.hu} width="8.5%">
                                     <Box>
                                         <Text>{score.score.ron === 0 ? "-" : score.score.ron}</Text>
-                                        <Text>{score.score.tumo === 0 ? "-" : score.score.tumo+"ALL"}</Text>                    
+                                        <Text>{score.score.tumo === 0 ? "-" : score.score.tumo+" オール"}</Text>                    
                                     </Box>
                                 </Table.Cell>
                             ))}
@@ -40,6 +41,8 @@ export default function Home() {
                     ))}
                 </Table.Body>
             </Table.Root>
+            </Table.ScrollArea>
+            <Table.ScrollArea borderWidth="1px" maxW="xl" width="90%">
             <Table.Root size="sm" variant="outline" showColumnBorder>
                 <Table.Header>
                     <Table.Row>
@@ -67,6 +70,7 @@ export default function Home() {
                     ))}
                 </Table.Body>
             </Table.Root>
+            </Table.ScrollArea>
         </Box>
     </Box>
   );
